810361b590beb7546843ed08aeec0b426f88e84d
Joao Meyer <1994meyer@gmail.com>
2020-02-17 23:26:43 -0300

    Minor refactor

--- table.js
+++ table.js
@@ -7,19 +7,77 @@ class Table {
         this.table = element
 
         console.debug(`Table: ${this.cols} cols, ${this.rows} rows`)
+        this._init()
+    }
 
+    _init () {
         // Rows
-        for (let i = 0; i < rows; i++) {
+        for (let i = 0; i < this.rows; i++) {
             const tr = document.createElement('tr')
             tr.id = i
-            element.appendChild(tr)
+            this.table.appendChild(tr)
 
             // Cols
-            for (let j = 0; j < cols; j++) {
+            for (let j = 0; j < this.cols; j++) {
                 const td = document.createElement('td')
                 td.id = j
                 tr.appendChild(td)
             }
+        }
+    }
+
+    move (x, y) {
+        if (x < 0)
+            this._move_right(-x)
+        if (x > 0)
+            this._move_left(x)
+        if (y < 0)
+            this._move_up(-y)
+        if (y > 0)
+            this._move_down(y)
+    }
+
+    _move_right (x) {
+        for (const tr of this.table.children) {
+            for (let i = 0; i < x; i++) {
+                const last = tr.lastChild
+                last.innerText = ''
+                tr.removeChild(last)
+                tr.prepend(last)
+            }
+        }
+    }
+
+    _move_left (x) {
+        for (const tr of this.table.children) {
+            for (let i = 0; i < x; i++) {
+                const first = tr.firstChild
+                first.innerText = ''
+                tr.removeChild(first)
+                tr.appendChild(first)
+            }
+        }
+    }
+
+    _move_up (y) {
+        for (let i = 0; i < y; i++) {
+            const tr = this.table.lastChild
+            for (const td of tr.children) {
+                td.innerText = ''
+            }
+            this.table.removeChild(tr)
+            this.table.prepend(tr)
+        }
+    }
+
+    _move_down (y) {
+        for (let i = 0; i < y; i++) {
+            const tr = this.table.firstChild
+            for (const td of tr.children) {
+                td.innerText = ''
+            }
+            this.table.removeChild(tr)
+            this.table.appendChild(tr)
         }
     }
 
