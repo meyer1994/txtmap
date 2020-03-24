e74aab9f53b8300fdff6363bc737d4f3a001f7b7
Joao Meyer <1994meyer@gmail.com>
2020-03-23 23:07:49 -0300

    Only one ENV

--- handler.py
+++ handler.py
@@ -2,12 +2,6 @@ import os
 
 from txtmap.handler import Handler
 
-DB_NAME = os.environ['DB_NAME']
-DB_HOST = os.environ['DB_HOST']
-DB_PORT = os.environ['DB_PORT']
-DB_USER = os.environ['DB_USER']
-DB_PASS = os.environ['DB_PASS']
+DB_URL = os.environ['DB_URL']
 
-URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-
-handler = Handler(URL)
+handler = Handler(DB_URL)
