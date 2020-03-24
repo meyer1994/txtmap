29e50ff540f2f25c8de76e012dada4d67201a504
Joao Meyer <1994meyer@gmail.com>
2020-03-23 21:57:15 -0300

    Created rotuer

---
+++ txtmap/router.py
@@ -0,0 +1,24 @@
+import inspect
+import logging
+from abc import ABC, abstractmethod
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+class Router(ABC):
+    def __init__(self):
+        super(Router, self).__init__()
+        methods = inspect.getmembers(self, predicate=inspect.ismethod)
+        self.methods = {k: v for k, v in methods if not k.startswith('_')}
+
+    def __call__(self, event, *args, **kwargs):
+        key = self.key(event)
+        logger.info('Route: %s', key)
+        function = self.methods[key]
+        return function(event, *args, **kwargs)
+
+    @staticmethod
+    @abstractmethod
+    def key(event):
+        pass
