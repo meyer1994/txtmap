b58b56c035d104565a9357bf26b9ada980019f2b
Joao Meyer <1994meyer@gmail.com>
2020-06-22 23:48:57 -0300

    Updated models names

--- txtmap/models.py
+++ txtmap/models.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel, constr, conlist
 
 
-class PostModel(BaseModel):
+class PostCoord(BaseModel):
     x: int
     y: int
     c: constr(min_length=1, max_length=1)
 
 
-class GetModel(BaseModel):
+class PostArea(BaseModel):
     a: conlist(int, min_items=2, max_items=2)
     b: conlist(int, min_items=2, max_items=2)
